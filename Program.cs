16face423d452bcb4d06e63b3e5789c85a8d11c7
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 03:34:05 +0545

    Updating the code to retrieve any value between one or more sets of parentheses

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿string message = "(What if) I am (only interested) in the last (set of parentheses)?";
-int openingPosition = message.LastIndexOf('(');
+﻿string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
 
-openingPosition += 1;
-int closingPosition = message.LastIndexOf(')');
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    message = message.Substring(closingPosition + 1);
+}
