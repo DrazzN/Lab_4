500be9fd504fe28ebec0f9a754302461e5725aea
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 03:59:55 +0545

    Solution for problem code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
-Console.WriteLine(message);
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = input;
+
+
+int openingPosition = output.IndexOf("<span>");
+int closingPosition = output.IndexOf("</span>");
+
+openingPosition += 6;
+int length = closingPosition - openingPosition;
+quantity = output.Substring(openingPosition, length);
+
+output = output.Replace("<div>", "");
+output = output.Replace("</div>", "");
+output = output.Replace("trade", "reg");
+
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
+
